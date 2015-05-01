5d181d2036c39e20118c7385938c65ab516365d7
Blake Johnson <blake41@gmail.com>
2015-05-01 11:38:09 -0400

    moved to new syntax

--- spec/myEach.spec.js
+++ spec/myEach.spec.js
@@ -1,12 +1,6 @@
 'use strict';
 
 describe('myEach', function() {
-
-  var myEach;
-  beforeEach(function() {
-    myEach = require('../lib/myEach');
-  });
-
   it('should call the function three times with each element of the array', function() {
     spyOn(console, 'log');
 
